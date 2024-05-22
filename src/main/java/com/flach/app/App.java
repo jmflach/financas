43cb18b8fe1503c0a7b39ceea3b31e4f565222c5
João Flach <joao.flach@serpro.gov.br>
2024-05-22 09:26:23 -0300

    iniciando os trabalhos

---
+++ src/main/java/com/flach/app/App.java
@@ -0,0 +1,13 @@
+package com.flach.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
